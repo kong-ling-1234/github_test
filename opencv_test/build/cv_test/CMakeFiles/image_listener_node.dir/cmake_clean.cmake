f0b7c85fce665336cc3bd4d0a87797852349ee82
kong-ling-1234 <2538891730@qq.com>
2023-07-16 02:49:56 -0400

    First commit

---
+++ opencv_test/build/cv_test/CMakeFiles/image_listener_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/image_listener_node.dir/src/image_listener.cpp.o"
+  "/home/klf/ros_ws/opencv_test/devel/lib/cv_test/image_listener_node.pdb"
+  "/home/klf/ros_ws/opencv_test/devel/lib/cv_test/image_listener_node"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/image_listener_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
